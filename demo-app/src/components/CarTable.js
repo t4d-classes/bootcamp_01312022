b6ba187cf49c38aef4459081f4346dd5bc650994
Eric Greene <eric@t4d.io>
2022-02-01 19:50:35 -0500

    add solution to exercise 7

--- demo-app/src/components/CarTable.js
+++ demo-app/src/components/CarTable.js
@@ -3,16 +3,24 @@ import { CarViewRow } from './CarViewRow';
 
 export const CarTable = props => {
 
+  const showSortDir = (colName) => {
+    if (props.carsSort.column === colName) {
+      return "(" + props.carsSort.direction + ")";
+    }
+  }
+
   return (
     <table>
       <thead>
         <tr>
-          <th>Id</th>
-          <th>Make</th>
-          <th>Model</th>
-          <th>Year</th>
-          <th>Color</th>
-          <th>Price</th>
+          <th><button onClick={() => props.onSortCars('id')}>
+            Id {showSortDir('id')}
+          </button></th>
+          <th><button onClick={() => props.onSortCars('make')}>Make {showSortDir('make')}</button></th>
+          <th><button onClick={() => props.onSortCars('model')}>Model {showSortDir('model')}</button></th>
+          <th><button onClick={() => props.onSortCars('year')}>Year {showSortDir('year')}</button></th>
+          <th><button onClick={() => props.onSortCars('color')}>Color {showSortDir('color')}</button></th>
+          <th><button onClick={() => props.onSortCars('price')}>Price {showSortDir('price')}</button></th>
         </tr>
       </thead>
       <tbody>
--- demo-app/src/components/CarTool.js
+++ demo-app/src/components/CarTool.js
@@ -1,12 +1,57 @@
+import { useState } from 'react';
+
 import { ToolHeader } from './ToolHeader';
 import { CarTable } from './CarTable';
 
+
+const sortCars = (cars, carsSort) => {
+
+  return [ ...cars ].sort((a,b) => {
+
+    if (a[carsSort.column] < b[carsSort.column]) {
+      return carsSort.direction === 'asc' ? -1 : 1;
+    } else if (a[carsSort.column] > b[carsSort.column]) {
+      return carsSort.direction === 'asc' ? 1 : -1;
+    } else {
+      return 0;
+    }
+
+  });
+
+};
+
 export const CarTool = (props) => {
+
+  const [ carsSort, setCarsSort ] = useState({
+    column: 'id',
+    direction: 'asc'
+  });
+
+  const doSortCars = (column) => {
+
+    if (column !== carsSort.column) {
+      setCarsSort({
+        column, direction: 'asc', 
+      });
+    } else {
+      if (carsSort.direction === 'asc') {
+        setCarsSort({
+          column, direction: 'desc', 
+        });
+      } else {
+        setCarsSort({
+          column, direction: 'asc', 
+        });
+      }
+    }
+
+  };
 
   return (
     <>
       <ToolHeader headerText="Car Tool" />
-      <CarTable cars={props.cars} />
+      <CarTable cars={sortCars(props.cars, carsSort)}
+        carsSort={carsSort} onSortCars={doSortCars} />
     </>
   );
 
