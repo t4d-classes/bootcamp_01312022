f0d5ce19a8faf83980ebcc1e990be7428b71452f
Eric Greene <eric@t4d.io>
2022-02-09 14:15:46 -0500

    add apollo exercise 1

--- apollo-app/server/src/resolvers.js
+++ apollo-app/server/src/resolvers.js
@@ -5,5 +5,19 @@ export const resolvers = {
     message() {
       return 'Welcome to React and Apollo!';
     },
+    age() {
+      return 23;
+    },
+    async color(_, args) {
+      const res = await fetch('http://localhost:5050/colors/' + args.id);
+      const color = await res.json();
+      return color;
+    },
+    colors() {
+      return [
+        { "id": 1, name: 'red', hexcode: 'ff0000' },
+        { id: 2, name: 'green', hexcode: '00ff00' },
+      ];
+    }
   },
 };
--- apollo-app/server/src/typeDefs.js
+++ apollo-app/server/src/typeDefs.js
@@ -3,5 +3,14 @@ import { gql } from 'apollo-server-express';
 export const typeDefs = gql`
   type Query {
     message: String
+    age: Int
+    color(id: ID): Color
+    colors: [Color]
+  }
+
+  type Color {
+    id: ID
+    name: String
+    hexcode: String
   }
 `;
